07f06259f200ac9779130aa8010d29071cdd0c39
Makovey <twomvlad@mail.ru>
2021-06-17 15:45:50 +0300

    [+] Added swagger and examples

--- src/main/java/org/makovej/bookshopapp/controllers/AuthorController.java
+++ src/main/java/org/makovej/bookshopapp/controllers/AuthorController.java
@@ -1,5 +1,7 @@
 package org.makovej.bookshopapp.controllers;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.makovej.bookshopapp.entities.Author;
 import org.makovej.bookshopapp.services.AuthorService;
@@ -7,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 import java.util.Map;
@@ -14,6 +17,7 @@ import java.util.Map;
 @Controller
 @RequestMapping("/authors")
 @RequiredArgsConstructor
+@Api(description = "authors data")
 public class AuthorController {
 
     private final AuthorService authorService;
@@ -26,5 +30,12 @@ public class AuthorController {
     @GetMapping
     public String getMainPage() {
         return "/authors/index";
+    }
+
+    @ApiOperation("method to get all authors")
+    @GetMapping("/api/author")
+    @ResponseBody
+    public Map<String, List<Author>> authorsMass(){
+        return authorService.getAuthorsMap();
     }
 }
